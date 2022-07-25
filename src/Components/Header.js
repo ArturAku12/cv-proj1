5db26bf2f7ee4d749d075f52ed557e6c1937952a
ArturAku12 <artur.akulenko@gmail.com>
2022-07-25 11:35:28 +0300

    quick one

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Info = (props) => {
     const { basic_info } = props;
     return (
         <div className="App-header">
-            <h1><u>CV</u></h1>
+            <h1><u>CVs</u></h1>
             <h2>{basic_info.name} </h2>
             <p>Email: {basic_info.email} &nbsp;&nbsp;&nbsp;<u> Phone: {basic_info.phone} </u></p>
         </div>
